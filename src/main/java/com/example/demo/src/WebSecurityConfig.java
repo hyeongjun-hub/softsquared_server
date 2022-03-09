bc665d03f2de523786259b766f4b2c3687c670af
hyeongjun <chogudwns@naver.com>
2022-03-09 10:29:47 +0900

    Update: Check spring security

--- src/main/java/com/example/demo/src/WebSecurityConfig.java
+++ src/main/java/com/example/demo/src/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.src;
 
+import com.example.demo.src.user.UserProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,8 +10,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+//                .authorizeRequests()
+//                .antMatchers("/restaurants/**", "/services", "/categories/**", "/menu/**", "/reviews/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .logout()
+//                .logoutUrl("/users/logout")
+//                .invalidateHttpSession(true)
+//                .permitAll();
     }
 }
